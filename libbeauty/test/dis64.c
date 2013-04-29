5aa9f4ff659763907bff47b0860c74ae6c06f025
James Courtier-Dutton <James@superbug.co.uk>
2013-04-29 21:23:02 +0100

    Add base_step to  path_node_to_base_path().

--- libbeauty/test/dis64.c
+++ libbeauty/test/dis64.c
@@ -2075,8 +2075,9 @@ int add_phi_to_node(struct control_flow_node_s *node, int reg)
 /* Input: path to search in.
  *        node to search for.
  * Output: common base_path that the node is part of.
+ *         common base_step that the node is part of.
  */
-int path_node_to_base_path(struct self_s *self, struct path_s *paths, int paths_size, int path, int node, int *base_path)
+int path_node_to_base_path(struct self_s *self, struct path_s *paths, int paths_size, int path, int node, int *base_path, int *base_step)
 {
 	int step;
 	int tmp;
@@ -2085,6 +2086,7 @@ int path_node_to_base_path(struct self_s *self, struct path_s *paths, int paths_
 	ret = 0;
 	*base_path = path;
 	step = paths[path].path_size - 1; /* convert size to index */
+	*base_step = step;
 	tmp = paths[path].path[step];
 	if (tmp == node) {
 		*base_path = path;
@@ -2108,6 +2110,7 @@ int path_node_to_base_path(struct self_s *self, struct path_s *paths, int paths_
 		tmp = paths[path].path[step];
 		if (tmp == node) {
 			*base_path = path;
+			*base_step = step;
 			ret = 0;
 			break;
 		}
