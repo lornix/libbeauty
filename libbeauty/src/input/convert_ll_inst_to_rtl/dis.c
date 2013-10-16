e495538077fbcd54e9692557ec20cca0b81539db
James Courtier-Dutton <James@superbug.co.uk>
2013-10-16 23:41:35 +0100

    FIXED: IF / conditional branched now correctly jump backwards by sign extending to 64bits.

--- libbeauty/src/input/convert_ll_inst_to_rtl/dis.c
+++ libbeauty/src/input/convert_ll_inst_to_rtl/dis.c
@@ -493,6 +493,7 @@ int convert_ll_inst_to_rtl(struct instruction_low_level_s *ll_inst, struct dis_i
 	int16_t rel16;
 	int32_t rel32;
 	int64_t rel64;
+	uint64_t value;
 	struct instruction_s *instruction;
 	dis_instructions->instruction_number = 0;
 	dis_instructions->bytes_used = ll_inst->octets;
@@ -521,7 +522,7 @@ int convert_ll_inst_to_rtl(struct instruction_low_level_s *ll_inst, struct dis_i
 		instruction->srcA.store = STORE_DIRECT;
 		instruction->srcA.indirect = IND_DIRECT;
 		instruction->srcA.indirect_size = 64;
-		uint64_t value = ll_inst->srcB.operand[0].value;
+		value = ll_inst->srcB.operand[0].value;
 		switch (ll_inst->srcB.operand[0].size) {
 		case 8:
 			rel8 = value;
@@ -567,7 +568,27 @@ int convert_ll_inst_to_rtl(struct instruction_low_level_s *ll_inst, struct dis_i
 		instruction->dstA.store = STORE_DIRECT;
 		instruction->dstA.indirect = IND_DIRECT;
 		instruction->dstA.indirect_size = 64;
-		instruction->dstA.index = ll_inst->srcB.operand[0].value;
+		value = ll_inst->srcB.operand[0].value;
+		switch (ll_inst->srcB.operand[0].size) {
+		case 8:
+			rel8 = value;
+			rel64 = rel8;
+			value = rel64;
+			break;
+		case 16:
+			rel16 = value;
+			rel64 = rel16;
+			value = rel64;
+			break;
+		case 32:
+			rel32 = value;
+			rel64 = rel32;
+			value = rel64;
+			break;
+		case 64:
+			break;
+		}
+		instruction->dstA.index = value;
 		instruction->dstA.relocated = 0;
 		instruction->dstA.value_size = 32;
 		instruction->srcA.store = STORE_DIRECT;
