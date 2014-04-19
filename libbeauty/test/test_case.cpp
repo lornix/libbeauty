c907ac2fa2476d2c13806549d0027e75e81d263d
James Courtier-Dutton <James@superbug.co.uk>
2014-04-19 10:54:16 +0100

    Add testing framework based around LLVM's "lit.py". Modifications to test_case.cpp contributed by Arun. Currently, only test_case.cpp has been converted to use lit. test_id will follow.
    Signed-off-by: James Courtier-Dutton <James@superbug.co.uk>
    Signed-off-by: Arun Prakash Jana <engineerarun@gmail.com>

--- libbeauty/test/test_case.cpp
+++ libbeauty/test/test_case.cpp
@@ -2,11 +2,18 @@
 #define __STDC_LIMIT_MACROS
 #define __STDC_CONSTANT_MACROS
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "llvm/MC/MCInstrInfo.h"
 #include "llvm/Support/Format.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/TargetSelect.h"
+
+#define MAX_INPUT_LEN 128
+#define MAX_TEST_DATA 16
 
 using namespace llvm;
 
@@ -45,6 +52,102 @@ int LLVMDecodeOpInfoCallback(void *DisInfo, uint64_t PC,
 	return 0;
 }
 
+/* getHexToken()
+   Get next valid hexadecimal token from str starting at pos
+   str is a string of hex tokens separated by 'delimiter'
+   *pos is updated to  the position of start of the next valid token before return
+*/
+
+uint8_t getHexToken(const char* str, char delimiter, int* pos)
+{
+	uint8_t hex = 0;
+	int start = 0;
+	const char* ptr = str + *pos;
+
+	while (*ptr && *ptr != delimiter) {
+		if (*ptr == 'x' || *ptr == 'X') {
+			start = 1; }
+		else if (!start && *ptr == '0') {
+		}
+		else if (!start) {
+			return 0; //TODO: add case for decimal value if needed
+		}
+		else {
+			if (*ptr >= '0' && *ptr <= '9')
+				hex = (hex << 4) | (*ptr - '0');
+			else if (*ptr >= 'A' && *ptr <= 'F')
+				hex = (hex << 4) | (*ptr - 'A' + 0xA);
+			else if (*ptr >= 'a' && *ptr <= 'f')
+				hex = (hex << 4) | (*ptr - 'a' + 0xa);
+			else if (*ptr == 'x' || *ptr == 'X');
+			else {
+				outs() << "Invalid character!\n";
+				exit(0);
+			}
+		}
+
+		++*pos;
+		ptr++;
+	}
+
+	if (!*ptr)
+		*pos = -1;
+	else
+		++*pos;
+
+	return hex;
+}
+
+/* analyzeLine()
+   Get a trimmed version of src in dst without consecutive spaces, tabs and '#'
+   Return the number of valid characters in dst without the NULL terminator
+*/
+
+int analyzeLine(const char* src, char* dst)
+{
+	const char* ptr = src;
+	int validchar = 0;
+	int tokens = 0;
+	char lastchar = '\0';
+
+	while (*ptr) {
+		if (*ptr == '#' || *ptr == '\n') {
+			if (validchar && dst[validchar - 1] == ' ')
+				validchar--;
+			break;
+		}
+
+		if (validchar == 0) {
+			if (*ptr == ' ' || *ptr == EOF || *ptr == '\t') {
+				ptr++;
+				continue;
+			}
+		}
+
+		if ((*ptr == ' ' || *ptr == '\t') && (lastchar == ' ' || lastchar == '\t')) {
+			ptr++;
+			continue;
+		}
+
+		if (lastchar == '\0' || lastchar == ' ' || lastchar == '\t')
+			tokens++;
+
+		if (*ptr == '\t')
+			dst[validchar++] = ' ';
+		else
+			dst[validchar++] = *ptr;
+
+		lastchar = *ptr;
+		ptr++;
+	}
+
+	dst[validchar] = '\0';
+
+	outs() << "validchar:" << validchar << "   tokens:" << tokens << "\n";
+
+	return tokens;
+}
+
 int main(int argc, char *argv[])
 {
 	int octets = 0;
@@ -53,33 +156,55 @@ int main(int argc, char *argv[])
 	uint8_t buffer1[1024];
 	uint8_t *buffer;
 	size_t buffer_size = 0;
-	uint8_t test_data[] = {0x0f, 0xb6, 0x75, 0xa0};
-	int test_data_size = 4;
+	char buf[MAX_INPUT_LEN] = {0};
+	char* line = NULL;
+	size_t bufsize;
+	ssize_t len = 0;
+	int pos = 0, i = 0;
+	int test_data_size = 0;
+	uint8_t test_data[MAX_TEST_DATA] = {0};
 
-	LLVMInitializeAllTargetInfos();
-	LLVMInitializeAllTargetMCs();
-	LLVMInitializeAllAsmParsers();
-	LLVMInitializeAllDisassemblers();
-
-	struct dis_info_s *dis_info = LLVMCreateDisInfo();
-	DC = LLVMCreateDisasm("x86_64-pc-linux-gnu", dis_info,
-		0, &LLVMDecodeOpInfoCallback,
-		NULL);
-	if (!DC) {
-		outs() << "LLVMCreateDisasm() failed\n";
-		exit(1);
+	FILE* fin = NULL;
+	if (argv[1]) {
+		if ((fin = fopen(argv[1], "r")) < 0) {
+			outs() << "usage: test_case filename\n" << argv[1];
+			exit(0);
+		}
 	}
 
-	buffer_size = test_data_size;
-	buffer = &(test_data[0]);
-	octets = LLVMDisasmInstruction(DC, buffer,
-		buffer_size, offset,
-		(char *)buffer1, 1023);
-	if (dis_info->size[0] == 1) {
-		outs() << "Test passed\n";
-	} else {
-		outs() << "Test failed\n";
+	while ((len = getline(&line, &bufsize, fin)) != -1) {
+		pos = 0;
+		test_data_size = analyzeLine(line, buf);
+
+		if (test_data_size == 0)
+			continue;
+
+		while (pos != -1) {
+			test_data[i] = getHexToken(buf, ' ', &pos);
+			outs() << "Test Data " << i << " [" << test_data[i] << "]\n";
+			i++;
+		}
+
+		LLVMInitializeAllTargetInfos();
+		LLVMInitializeAllTargetMCs();
+		LLVMInitializeAllAsmParsers();
+		LLVMInitializeAllDisassemblers();
+
+		struct dis_info_s *dis_info = LLVMCreateDisInfo();
+		DC = LLVMCreateDisasm("x86_64-pc-linux-gnu", dis_info,
+			0, &LLVMDecodeOpInfoCallback,
+			NULL);
+		if (!DC) {
+			outs() << "LLVMCreateDisasm() failed\n";
+			exit(1);
+		}
+
+		buffer_size = test_data_size;
+		buffer = &(test_data[0]);
+		octets = LLVMDisasmInstruction(DC, buffer,
+			buffer_size, offset,
+			(char *)buffer1, 1023);
 	}
+
 	return 0;
-
 }
